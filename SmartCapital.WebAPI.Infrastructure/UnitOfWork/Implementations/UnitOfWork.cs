d9f85d68210a7eaefbb8030f34e08551371bfc69
SAMI_DANIEL_SANTOS_SILVA <22201491@ALFAF52.FLORESTA>
2024-08-05 08:07:23 -0300

    Criar UnitOfWork

---
+++ SmartCapital.WebAPI.Infrastructure/UnitOfWork/Implementations/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using SmartCapital.WebAPI.Infrastructure.Data.Contexts;
+using SmartCapital.WebAPI.Infrastructure.Repository.Interfaces;
+using SmartCapital.WebAPI.Infrastructure.UnitOfWork.Interfaces;
+
+namespace SmartCapital.WebAPI.Infrastructure.UnitOfWork.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private bool disposedValue;
+
+        public IProfileRepository ProfileRepository { get; }
+
+        public UnitOfWork(IProfileRepository profileRepository, ApplicationDbContext applicationDbContext)
+        {
+            ProfileRepository = profileRepository;
+            _context = applicationDbContext;
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ SmartCapital.WebAPI.Infrastructure/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using SmartCapital.WebAPI.Infrastructure.Repository.Interfaces;
+
+namespace SmartCapital.WebAPI.Infrastructure.UnitOfWork.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IProfileRepository ProfileRepository { get; }
+        public Task<int> CompleteAsync();
+    }
+}
