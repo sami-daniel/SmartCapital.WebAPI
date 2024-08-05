1bc843d25bbb663d160eee72e68e18f6f5146f34
SAMI_DANIEL_SANTOS_SILVA <22201491@ALFAF52.FLORESTA>
2024-08-05 07:58:31 -0300

    Adicionar implementação basica de IProfileRepository

---
+++ SmartCapital.WebAPI.Infrastructure/Repository/Implementations/ProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using SmartCapital.WebAPI.Domain.Domain;
+using SmartCapital.WebAPI.Infrastructure.Data.Contexts;
+using SmartCapital.WebAPI.Infrastructure.Repository.Core.Implementations;
+using SmartCapital.WebAPI.Infrastructure.Repository.Interfaces;
+
+namespace SmartCapital.WebAPI.Infrastructure.Repository.Implementations
+{
+    public class ProfileRepository : Repository<Profile> ,IProfileRepository
+    {
+        public ProfileRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
