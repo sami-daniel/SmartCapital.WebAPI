6e60460c6585d6c2f2b49bd42ec0a304c6668a25
Sami Daniel <samidaniel125@gmail.com>
2024-08-05 21:19:17 -0300

    Remover método OnConfiguring (sera configurado na camada de exibição, para o Container DI atuar)

--- SmartCapital.WebAPI.Infrastructure/Data/Contexts/ApplicationDbContext.cs
+++ SmartCapital.WebAPI.Infrastructure/Data/Contexts/ApplicationDbContext.cs
@@ -26,10 +26,6 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<SavingsResult> SavingsResults { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;uid=root;database=smartcapitaldatabase;pwd=saminho123", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.4.0-mysql"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
