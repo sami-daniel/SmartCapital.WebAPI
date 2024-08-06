1a34af168f1fcd8698a58411285113ea7800e51f
Sami Daniel <samidaniel125@gmail.com>
2024-08-05 21:58:09 -0300

    Criar ExistingProfileException

---
+++ SmartCapital.WebAPI.Application/Exceptions/ExistingProfileException.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace SmartCapital.WebAPI.Application.Exceptions
+{
+    public class ExistingProfileException : Exception
+    {
+        public ExistingProfileException()
+        {
+        }
+
+        public ExistingProfileException(string? message) : base(message)
+        {
+        }
+
+        public ExistingProfileException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
