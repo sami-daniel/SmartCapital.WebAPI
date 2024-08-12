15540b51cef615b9317ee6a7f425fb3875ed31ac
SAMI_DANIEL_SANTOS_SILVA <22201491@ALFAF52.FLORESTA>
2024-08-12 12:14:21 -0300

    Adptando os controladores as novas modificações

--- SmartCapital.WebAPI/Controllers/ProfilesController.cs
+++ SmartCapital.WebAPI/Controllers/ProfilesController.cs
@@ -3,6 +3,7 @@ using SmartCapital.WebAPI.Application.DTO.AddRequests;
 using SmartCapital.WebAPI.Application.Exceptions;
 using SmartCapital.WebAPI.Application.Interfaces;
 using SmartCapital.WebAPI.DTO.Responses;
+using SmartCapital.WebAPI.Models;
 
 namespace SmartCapital.WebAPI.Controllers
 {
@@ -54,7 +55,11 @@ namespace SmartCapital.WebAPI.Controllers
                 return Ok(profiles.First().ToProfileResponse());
             }
 
-            return NotFound();
+            return NotFound(new ErrorResponse
+            {
+                ErrorType = "ProfileNotFound",
+                Message = "O perfil com o nome fornecido não pode ser encontrado."
+            });
         }
 
         /// <summary>
@@ -63,7 +68,10 @@ namespace SmartCapital.WebAPI.Controllers
         /// <param name="profile">Os dados do perfil a ser criado.</param>
         /// <returns>O perfil recentemente criado.</returns>
         /// <response code="201">O perfil foi criado com sucesso.</response>
-        /// <response code="400">Houve um erro de validação nos dados do perfil.</response>
+        /// <response code="400">
+        /// Houve um erro na criação do perfil:
+        /// - Se o erro for de validação de dados, o campos fornecidos no esquema são invalidos. Confira https://localhost:7063/swagger/v1/swagger.json > components > ProfileAddRequest para verificar o formato dos campos.
+        /// </response>
         [HttpPost]
         [ProducesResponseType(201)]
         public async Task<IActionResult> AddProfile([FromBody] ProfileAddRequest profile)
@@ -74,11 +82,19 @@ namespace SmartCapital.WebAPI.Controllers
             }
             catch (ArgumentException ex)
             {
-                return BadRequest($"Erro de validação de dados: {ex.Message}");
+                return BadRequest(new ErrorResponse
+                {
+                    ErrorType = "ValidationError",
+                    Message = $"Erro de validação de dados: {ex.Message}"
+                });
             }
             catch (ExistingProfileException ex)
             {
-                return BadRequest(ex.Message);
+                return BadRequest(new ErrorResponse
+                {
+                    ErrorType = "ProfileCreationError",
+                    Message = $"Erro na criação do Perfil: {ex.Message}"
+                });
             }
 
             return CreatedAtRoute("", new { profile.ProfileName });
@@ -87,18 +103,22 @@ namespace SmartCapital.WebAPI.Controllers
         /// <summary>
         /// Deleta um perfil com o ID especificado.
         /// </summary>
-        /// <param name="ID">O ID do perfil a ser deletado.</param>
+        /// <param name="profileName">O nome do perfil a ser deletado.</param>
         /// <returns>Um status de resposta indicando o resultado da operação.</returns>
         /// <response code="204">O perfil foi deletado com sucesso.</response>
         /// <response code="404">Não foi possível encontrar um perfil com o ID especificado.</response>
         [HttpDelete("{ID:int}")]
         [ProducesResponseType(204)]
-        public async Task<IActionResult> DeleteProfile([FromRoute] uint ID)
+        public async Task<IActionResult> DeleteProfile([FromRoute] string profileName)
         {
-            var profileToRemove = await _profileService.GetProfileByIDAsync(ID);
+            var profileToRemove = await _profileService.GetProfileByNameAsync(profileName);
 
             if (profileToRemove == null)
-                return NotFound();
+                return NotFound(new ErrorResponse
+                {
+                    ErrorType = "ProfileNotFound",
+                    Message = "O perfil com o nome fornecido não pode ser encontrado."
+                });
 
             await _profileService.RemoveProfileAsync(profileToRemove);
 
