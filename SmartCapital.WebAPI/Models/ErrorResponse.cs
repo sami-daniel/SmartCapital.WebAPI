78723ad0268987ad9326034da8ea63f9feaedd56
SAMI_DANIEL_SANTOS_SILVA <22201491@ALFAF52.FLORESTA>
2024-08-12 07:59:49 -0300

    Adicionar modelo de resposta usado para erros nos endpoints da API

---
+++ SmartCapital.WebAPI/Models/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartCapital.WebAPI.Models
+{
+    /// <summary>
+    /// Modelo usado como resposta para erros nos endpoints dos controladores.
+    /// </summary>
+    public class ErrorResponse
+    {
+        public required string ErrorType { get; set; }
+        public required string Message { get; set; }
+    }
+}
