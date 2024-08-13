ba53dc7bae716f69e6a7fc5b267fa13f06a26f2c
SAMI_DANIEL_SANTOS_SILVA <22201491@OMEGAF06.FLORESTA>
2024-08-13 16:17:18 -0300

    Adicionar filtros de esquemas do swagger na aplicação

---
+++ SmartCapital.WebAPI/Swagger/Filters/RemoveSchemaDocumentFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SmartCapital.WebAPI.Swagger.Filters
+{
+    public class RemoveSchemaDocumentFilter : IDocumentFilter
+    {
+        private readonly string _schemaNameToRemove;
+
+        public RemoveSchemaDocumentFilter(string schemaNameToRemove)
+        {
+            _schemaNameToRemove = schemaNameToRemove;
+        }
+
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            if (swaggerDoc.Components.Schemas.ContainsKey(_schemaNameToRemove))
+            {
+                swaggerDoc.Components.Schemas.Remove(_schemaNameToRemove);
+            }
+        }
+    }
+}
