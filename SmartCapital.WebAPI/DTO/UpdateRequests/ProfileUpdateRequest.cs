2be31752ab59fe1f7b407a26cd4d4293f5416ad9
Sami Daniel <samidaniel125@gmail.com>
2024-08-25 16:30:42 -0300

    Criar DTO ProfileUpdateRequest

---
+++ SmartCapital.WebAPI/DTO/UpdateRequests/ProfileUpdateRequest.cs
@@ -0,0 +1,42 @@
+﻿using SmartCapital.WebAPI.Application.DTO.AddRequests;
+using SmartCapital.WebAPI.Domain.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartCapital.WebAPI.DTO.UpdateRequests
+{
+    public class ProfileUpdateRequest
+    {
+        /// <summary>
+        /// Nome do perfil. Deve ter no máximo 255 caracteres.
+        /// </summary>
+        /// <remarks>
+        /// Este campo é obrigatório e não pode ser nulo.
+        /// </remarks>
+        [Required]
+        [StringLength(255, ErrorMessage = "O tamanho do Nome do Perfil não pode exceder {0} caracteres")]
+        [RegularExpression("^[a-zA-Z0-9 ]*$", ErrorMessage = "O Nome do Perfil pode conter somente letras e números.")]
+        public string ProfileName { get; set; } = null!;
+
+        /// <summary>
+        /// Saldo inicial do perfil. Deve estar dentro do intervalo permitido de 0 a 999.999.999,99.
+        /// </summary>
+        /// <remarks>
+        /// Este campo é opcional e pode ser nulo.
+        /// </remarks>
+        [Range(0d, 999_999_999.99d, ErrorMessage = "O tamanho do Saldo Inicial do Perfil não pode ser maior que {1}.")]
+        public decimal? ProfileOpeningBalance { get; set; }
+
+        /// <summary>
+        /// Converte a instância atual de <see cref="ProfileUpdateRequest"/> em uma instância de <see cref="Profile"/>.
+        /// </summary>
+        /// <returns>Uma nova instância de <see cref="Profile"/> com os dados do perfil.</returns>
+        public Profile ToProfile()
+        {
+            return new Profile
+            {
+                ProfileName = ProfileName,
+                ProfileOpeningBalance = ProfileOpeningBalance
+            };
+        }
+    }
+}
