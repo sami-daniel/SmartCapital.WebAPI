b340f52471c75e8a3edc6da523a70c645e0f12d7
Sami Daniel <samidaniel125@gmail.com>
2024-08-26 20:35:15 -0300

    Criar interface para o repositorio do usuario

---
+++ SmartCapital.WebAPI.Infrastructure/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using SmartCapital.WebAPI.Domain.Domain;
+using SmartCapital.WebAPI.Infrastructure.Repository.Core.Interfaces;
+
+namespace SmartCapital.WebAPI.Infrastructure.Repository.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
