4a3200ba6785b06dce65c7f991224ef05f9f6f86
Sami Daniel <samidaniel125@gmail.com>
2024-08-26 20:08:21 -0300

    Adicionar entidade User no DbContext

--- SmartCapital.WebAPI.Infrastructure/Data/Contexts/ApplicationDbContext.cs
+++ SmartCapital.WebAPI.Infrastructure/Data/Contexts/ApplicationDbContext.cs
@@ -26,6 +26,8 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<SavingsResult> SavingsResults { get; set; }
 
+    public virtual DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
@@ -73,6 +75,7 @@ public partial class ApplicationDbContext : DbContext
                 .HasConstraintName("fk_Expenses_Category");
 
             entity.HasOne(d => d.Profile).WithMany(p => p.Expenses)
+                .HasPrincipalKey(p => p.ProfileId)
                 .HasForeignKey(d => d.ProfileId)
                 .HasConstraintName("fk_Expenses_Profiles");
         });
@@ -105,20 +108,35 @@ public partial class ApplicationDbContext : DbContext
                 .HasConstraintName("fk_Incomes_Category");
 
             entity.HasOne(d => d.Profile).WithMany(p => p.Incomes)
+                .HasPrincipalKey(p => p.ProfileId)
                 .HasForeignKey(d => d.ProfileId)
                 .HasConstraintName("fk_Incomes_Profiles");
         });
 
         modelBuilder.Entity<Profile>(entity =>
         {
-            entity.HasKey(e => e.ProfileId).HasName("PRIMARY");
+            entity.HasKey(e => new { e.ProfileId, e.UsersUserId })
+                .HasName("PRIMARY")
+                .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
 
             entity.ToTable("profiles");
 
-            entity.Property(e => e.ProfileId).HasColumnName("ProfileID");
+            entity.HasIndex(e => e.ProfileId, "ProfileID_UNIQUE").IsUnique();
+
+            entity.HasIndex(e => e.UsersUserId, "fk_Profiles_Users1_idx");
+
+            entity.Property(e => e.ProfileId)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("ProfileID");
+            entity.Property(e => e.UsersUserId).HasColumnName("Users_UserID");
             entity.Property(e => e.ProfileCreationDate).HasColumnType("datetime");
             entity.Property(e => e.ProfileName).HasMaxLength(255);
             entity.Property(e => e.ProfileOpeningBalance).HasPrecision(12, 2);
+
+            entity.HasOne(d => d.UsersUser).WithMany(p => p.Profiles)
+                .HasForeignKey(d => d.UsersUserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_Profiles_Users1");
         });
 
         modelBuilder.Entity<SavingsResult>(entity =>
@@ -132,6 +150,19 @@ public partial class ApplicationDbContext : DbContext
             entity.Property(e => e.TotalEconomy).HasPrecision(35, 2);
             entity.Property(e => e.TotalExpense).HasPrecision(34, 2);
             entity.Property(e => e.TotalIncome).HasPrecision(34, 2);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.UserName, "UserName_UNIQUE").IsUnique();
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.UserCreationDate).HasColumnType("datetime");
+            entity.Property(e => e.UserPassword).HasMaxLength(255);
         });
 
         OnModelCreatingPartial(modelBuilder);
