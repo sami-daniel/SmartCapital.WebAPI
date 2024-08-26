ade575eb57eff76102406b9f0825a0cf46184167
Sami Daniel <samidaniel125@gmail.com>
2024-08-26 20:37:32 -0300

    Criar implementação da interface de repositorio de usuario

---
+++ SmartCapital.WebAPI.Infrastructure/Repository/Implementations/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using SmartCapital.WebAPI.Domain.Domain;
+using SmartCapital.WebAPI.Infrastructure.Data.Contexts;
+using SmartCapital.WebAPI.Infrastructure.Repository.Core.Implementations;
+using SmartCapital.WebAPI.Infrastructure.Repository.Interfaces;
+
+namespace SmartCapital.WebAPI.Infrastructure.Repository.Implementations
+{
+    /// <summary>
+    /// Implementa um repositório específico para a entidade <see cref="User"/>.
+    /// </summary>
+    public class UserRepository : Repository<User> ,IUserRepository
+    {
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UserRepository"/> com o contexto de banco de dados fornecido.
+        /// </summary>
+        /// <param name="applicationDbContext">O contexto de banco de dados usado para acessar a fonte de dados.</param>
+        public UserRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
