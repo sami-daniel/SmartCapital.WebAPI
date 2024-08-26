48b3ebda46b874541ad00c827fe177dcb135e966
Sami Daniel <samidaniel125@gmail.com>
2024-08-26 20:07:42 -0300

    Criar tabela User e adicionar referencia do User a um perfil

--- SmartCapital.WebAPI.Domain/Domain/Profile.cs
+++ SmartCapital.WebAPI.Domain/Domain/Profile.cs
@@ -13,7 +13,11 @@ public partial class Profile
 
     public decimal? ProfileOpeningBalance { get; set; }
 
+    public uint UsersUserId { get; set; }
+
     public virtual ICollection<Expense> Expenses { get; set; } = new List<Expense>();
 
     public virtual ICollection<Income> Incomes { get; set; } = new List<Income>();
+
+    public virtual User UsersUser { get; set; } = null!;
 }
---
+++ SmartCapital.WebAPI.Domain/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartCapital.WebAPI.Domain.Domain;
+
+public partial class User
+{
+    public uint UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+
+    public DateTime UserCreationDate { get; set; }
+
+    public virtual ICollection<Profile> Profiles { get; set; } = new List<Profile>();
+}
