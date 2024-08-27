4af575e65b691dd7db518f6d211fa415e63f1979
SAMI_DANIEL_SANTOS_SILVA <22201491@OMEGAF06.FLORESTA>
2024-08-27 16:02:18 -0300

    Adicionar UserLoginResponse

---
+++ SmartCapital.WebAPI/DTO/Login/UserLoginResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace SmartCapital.WebAPI.DTO.Login
+{
+    /// <summary>
+    /// Representa a resposta de autenticação do usuário, contendo informações sobre o usuário autenticado e o token JWT gerado.
+    /// </summary>
+    public class UserLoginResponse
+    {
+        /// <summary>
+        /// O nome do usuário autenticado.
+        /// </summary>
+        /// <example>user@example.com</example>
+        public string User { get; set; } = null!;
+
+        /// <summary>
+        /// O papel ou a função do usuário no sistema.
+        /// </summary>
+        /// <example>APIUser</example>
+        public string Role { get; set; } = null!;
+
+        /// <summary>
+        /// O token JWT gerado para o usuário autenticado.
+        /// </summary>
+        /// <example>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...</example>
+        public string Token { get; set; } = null!;
+    }
+}
