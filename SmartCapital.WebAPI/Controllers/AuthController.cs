733de7692dfcc90323c511d7b3f287e7fc41e013
SAMI_DANIEL_SANTOS_SILVA <22201491@OMEGAF06.FLORESTA>
2024-08-27 15:23:15 -0300

    Adicionar documentação no controlador de autenticação

--- SmartCapital.WebAPI/Controllers/AuthController.cs
+++ SmartCapital.WebAPI/Controllers/AuthController.cs
@@ -23,16 +23,30 @@ namespace SmartCapital.WebAPI.Controllers
         private readonly IConfiguration _configuration;
         private readonly IUserService _userService;
 
+        /// <summary>
+        /// Inicializa uma nova instância de <see cref="AuthController"/> com o serviço de usuários fornecido.
+        /// </summary>
+        /// <param name="userService">Serviço para gerenciar operações de usuários.</param>
+        /// <param name="configuration">Set de configurações do projeto</param>
         public AuthController(IConfiguration configuration, IUserService userService)
         {
             _configuration = configuration;
             _userService = userService;
         }
 
+        /// <summary>
+        /// Autentica um usuário e gera um token JWT se as credenciais forem válidas.
+        /// </summary>
+        /// <param name="userLoginRequest">O objeto contendo as credenciais de login do usuário.</param>
+        /// <returns>Retorna um resultado de ação contendo o nome do usuário e o token JWT se a autenticação for bem-sucedida. Caso contrário, retorna um erro se o usuário não for encontrado.</returns>
+        /// <response code="200">Retorna o nome do usuário, a Role e o token JWT.</response>
+        /// <response code="404">Retorna um erro se o usuário com o nome especificado não for encontrado.</response>
+        /// <response code="400">Retorna um erro se a solicitação estiver malformada.</response>
         [HttpPost]
-        [Route("login")]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login([FromBody] UserLoginRequest userLoginRequest)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ErrorResponse), 404)]
+        [ProducesResponseType(typeof(ErrorResponse), 400)]
+        public async Task<IActionResult> Authenticate([FromBody] UserLoginRequest userLoginRequest)
         {
             var user = await _userService.GetProfileByNameAsync(userLoginRequest.UserName!);
 
@@ -49,39 +63,17 @@ namespace SmartCapital.WebAPI.Controllers
             return Ok(new
             {
                 User = user.UserName,
+                Role = User.Claims.First(u => u.ValueType == ClaimTypes.Role).Value,
                 Token = token
             });
         }
 
-        [HttpPost]
-        [Route("register")]
-        [AllowAnonymous]
-        public async Task<IActionResult> Register([FromBody] UserAddRequest userAddRequest)
-        {
-            try
-            {
-                await _userService.AddUserAsync(userAddRequest.ToUser());
-            }
-            catch (ArgumentException ex) 
-            {
-                return BadRequest(new ErrorResponse
-                {
-                    ErrorType = "ValidationError",
-                    Message = $"Ocorreu um erro de validação de dados: {ex.Message}"
-                });
-            }
-            catch (ExistingUserException ex)
-            {
-                return BadRequest(new ErrorResponse
-                {
-                    ErrorType = "UserCreationError",
-                    Message = $"Ocorreu um erro na criação do usuário: {ex.Message}"
-                });
-            }
-
-            return Created();
-        }
-
+        /// <summary>
+        /// Gera um token JWT para o usuário fornecido.
+        /// </summary>
+        /// <param name="user">O usuário para o qual o token JWT será gerado.</param>
+        /// <returns>O token JWT gerado.</returns>
+        /// <exception cref="InvalidOperationException">Lançado quando a configuração do segredo JWT está ausente.</exception>
         [NonAction]
         public string GenerateToken(UserResponse user)
         {
@@ -91,8 +83,8 @@ namespace SmartCapital.WebAPI.Controllers
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.UserName),
-                    new Claim(ClaimTypes.Role, "APIUser")
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Role, "APIUser")
                 }),
                 Expires = DateTime.UtcNow.AddDays(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
