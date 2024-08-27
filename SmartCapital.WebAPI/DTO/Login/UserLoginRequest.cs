eb5a96bfb80215207ca1c64e8703d61da25c97e7
SAMI_DANIEL_SANTOS_SILVA <22201491@OMEGAF06.FLORESTA>
2024-08-27 16:01:59 -0300

    Docs

--- SmartCapital.WebAPI/DTO/Login/UserLoginRequest.cs
+++ SmartCapital.WebAPI/DTO/Login/UserLoginRequest.cs
@@ -2,14 +2,25 @@
 
 namespace SmartCapital.WebAPI.DTO.Login
 {
+    /// <summary>
+    /// Representa a solicitação de login do usuário, contendo as credenciais necessárias para autenticação.
+    /// </summary>
     public class UserLoginRequest
     {
+        /// <summary>
+        /// O nome do usuário usado para autenticação. Deve ser fornecido e não pode estar vazio.
+        /// </summary>
+        /// <example>user@example.com</example>
         [Required(ErrorMessage = "O Nome do Usuário não pode ser vazio.")]
         [StringLength(255, ErrorMessage = "O Nome do Usuário não pode exceder {0} caracteres.")]
         public string UserName { get; set; } = null!;
 
+        /// <summary>
+        /// A senha do usuário usada para autenticação. Deve ser fornecida e não pode estar vazia.
+        /// </summary>
+        /// <example>yourpassword123</example>
         [Required(ErrorMessage = "A Senha do Usuário não pode ser vazia.")]
-        [StringLength(255, ErrorMessage = "A Senha do Usuário não pode ser vazia.")]
+        [StringLength(255, ErrorMessage = "A Senha do Usuário não pode exceder {0} caracteres.")]
         public string UserPassword { get; set; } = null!;
     }
 }
