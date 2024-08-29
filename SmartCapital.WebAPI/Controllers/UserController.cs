a73726911f5f0bf9a106f24453ac1f75935d1353
Sami Daniel <samidaniel125@gmail.com>
2024-08-29 19:52:52 -0300

    Refactor UserController for readability and role checks
    Re-added XML documentation comments for AddUser, DeleteUser, and UpdateUser methods. Refactored AddUser to handle exceptions directly within the method body. Added role checks in DeleteUser and UpdateUser to allow actions by admins. Updated DeleteUser and UpdateUser to return appropriate HTTP responses for various error conditions. Improved overall readability and maintainability of the code.

--- SmartCapital.WebAPI/Controllers/UserController.cs
+++ SmartCapital.WebAPI/Controllers/UserController.cs
@@ -82,134 +82,137 @@ namespace SmartCapital.WebAPI.Controllers
             });
 
         }
-            /// <summary>
-            /// Cria um novo usuário no sistema.
-            /// </summary>
-            /// <param name="user">Dados do usuário a ser criado.</param>
-            /// <returns>O usuário recém-criado.</returns>
-            /// <response code="201">Usuário criado com sucesso.</response>
-            /// <response code="400">Erro ao criar o usuário, devido a problemas de validação ou duplicidade.</response>
+        /// <summary>
+        /// Cria um novo usuário no sistema.
+        /// </summary>
+        /// <param name="user">Dados do usuário a ser criado.</param>
+        /// <returns>O usuário recém-criado.</returns>
+        /// <response code="201">Usuário criado com sucesso.</response>
+        /// <response code="400">Erro ao criar o usuário, devido a problemas de validação ou duplicidade.</response>
 
-            [HttpPost]
-            [AllowAnonymous]
-            [ProducesResponseType(typeof(UserResponse), 201)]
-            [ProducesResponseType(typeof(ErrorResponse), 400)]
-            public async Task<IActionResult> AddUser([FromBody] UserAddRequest user)
+        [HttpPost]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(UserResponse), 201)]
+        [ProducesResponseType(typeof(ErrorResponse), 400)]
+        public async Task<IActionResult> AddUser([FromBody] UserAddRequest user)
+        {
+            try
             {
-                try
+                await _userService.AddUserAsync(user.ToUser());
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new ErrorResponse
                 {
-                    await _userService.AddUserAsync(user.ToUser());
-                }
-                catch (ArgumentException ex)
+                    ErrorType = "ValidationError",
+                    Message = $"Erro de validação: {ex.Message}"
+                });
+            }
+            catch (ExistingUserException ex)
+            {
+                return BadRequest(new ErrorResponse
                 {
-                    return BadRequest(new ErrorResponse
-                    {
-                        ErrorType = "ValidationError",
-                        Message = $"Erro de validação: {ex.Message}"
-                    });
-                }
-                catch (ExistingUserException ex)
-                {
-                    return BadRequest(new ErrorResponse
-                    {
-                        ErrorType = "UserCreationError",
-                        Message = $"Erro ao criar o usuário: {ex.Message}"
-                    });
-                }
-
-                return CreatedAtRoute("", new { user.UserName });
+                    ErrorType = "UserCreationError",
+                    Message = $"Erro ao criar o usuário: {ex.Message}"
+                });
             }
 
-            /// <summary>
-            /// Exclui o usuário correspondente ao nome fornecido.
-            /// </summary>
-            /// <param name="userName">Nome do usuário a ser excluído.</param>
-            /// <returns>Status indicando o resultado da operação.</returns>
-            /// <response code="204">Usuário excluído com sucesso.</response>
-            /// <response code="403">Não autorizado o acesso ao recurso.</response>
-            /// <response code="404">Usuário com o nome fornecido não encontrado.</response>
-            [HttpDelete("{userName}")]
-            [ProducesResponseType(204)]
-            [ProducesResponseType(403)]
-            [ProducesResponseType(typeof(ErrorResponse), 404)]
-            public async Task<IActionResult> DeleteUser([FromRoute] string userName)
+            return CreatedAtRoute("", new { user.UserName });
+        }
+
+        /// <summary>
+        /// Exclui o usuário correspondente ao nome fornecido.
+        /// </summary>
+        /// <param name="userName">Nome do usuário a ser excluído.</param>
+        /// <returns>Status indicando o resultado da operação.</returns>
+        /// <response code="204">Usuário excluído com sucesso.</response>
+        /// <response code="403">Não autorizado o acesso ao recurso.</response>
+        /// <response code="404">Usuário com o nome fornecido não encontrado.</response>
+        [HttpDelete("{userName}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(typeof(ErrorResponse), 404)]
+        public async Task<IActionResult> DeleteUser([FromRoute] string userName)
+        {
+            var userToRemove = await _userService.GetUserByNameAsync(userName);
+
+            var userNameFromToken = HttpContext.Items["User"] as string;
+            var role = HttpContext.Items["Role"] as string;
+
+            if (userNameFromToken != userName && role != "Admin")
             {
-                var userToRemove = await _userService.GetUserByNameAsync(userName);
+                return Forbid();
+            }
 
-                var userNameFromToken = HttpContext.Items["User"] as string;
+            if (userToRemove == null)
+                return NotFound(new ErrorResponse
+                {
+                    ErrorType = "UserNotFound",
+                    Message = "O usuário com o nome fornecido não foi encontrado."
+                });
 
-                if (userToRemove == null)
-                    return NotFound(new ErrorResponse
-                    {
-                        ErrorType = "UserNotFound",
-                        Message = "O usuário com o nome fornecido não foi encontrado."
-                    });
 
-                if (userNameFromToken != userToRemove.UserName)
+            await _userService.RemoveUserAsync(userToRemove);
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Atualiza um usuário existente com base no nome fornecido.
+        /// </summary>
+        /// <param name="userName">Nome do usuário a ser atualizado.</param>
+        /// <param name="user">Dados atualizados do usuário.</param>
+        /// <returns>Status indicando o resultado da operação.</returns>
+        /// <response code="204">Usuário atualizado com sucesso.</response>
+        /// <response code="400">Erro na atualização do usuário, devido a problemas de validação ou duplicidade.</response>
+        /// <response code="403">Não autorizado o acesso ao recurso.</response>
+        /// <response code="404">Usuário com o nome fornecido não encontrado.</response>
+        [HttpPut("{userName}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(typeof(ErrorResponse), 400)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(typeof(ErrorResponse), 404)]
+        public async Task<IActionResult> UpdateUser([FromRoute] string userName, [FromBody] UserUpdateRequest user)
+        {
+            User? updatedUser;
+
+            var userNameFromToken = HttpContext.Items["User"] as string;
+            var role = HttpContext.Items["Role"] as string;
+
+            try
+            {
+                if (userNameFromToken != userName && role != "Admin")
                 {
                     return Forbid();
                 }
 
-                await _userService.RemoveUserAsync(userToRemove);
-
-                return NoContent();
+                updatedUser = await _userService.UpdateUserAsync(userName, user.ToUser());
             }
-
-            /// <summary>
-            /// Atualiza um usuário existente com base no nome fornecido.
-            /// </summary>
-            /// <param name="userName">Nome do usuário a ser atualizado.</param>
-            /// <param name="user">Dados atualizados do usuário.</param>
-            /// <returns>Status indicando o resultado da operação.</returns>
-            /// <response code="204">Usuário atualizado com sucesso.</response>
-            /// <response code="400">Erro na atualização do usuário, devido a problemas de validação ou duplicidade.</response>
-            /// <response code="403">Não autorizado o acesso ao recurso.</response>
-            /// <response code="404">Usuário com o nome fornecido não encontrado.</response>
-            [HttpPut("{userName}")]
-            [ProducesResponseType(204)]
-            [ProducesResponseType(typeof(ErrorResponse), 400)]
-            [ProducesResponseType(403)]
-            [ProducesResponseType(typeof(ErrorResponse), 404)]
-            public async Task<IActionResult> UpdateUser([FromRoute] string userName, [FromBody] UserUpdateRequest user)
+            catch (ArgumentException ex)
             {
-                User? updatedUser;
-
-                var userNameFromToken = HttpContext.Items["User"] as string;
-
-                try
+                return BadRequest(new ErrorResponse
                 {
-                    if (userNameFromToken != userName)
-                    {
-                        return Forbid();
-                    }
-
-                    updatedUser = await _userService.UpdateUserAsync(userName, user.ToUser());
-                }
-                catch (ArgumentException ex)
-                {
-                    return BadRequest(new ErrorResponse
-                    {
-                        ErrorType = "ValidationError",
-                        Message = $"Erro de validação: {ex.Message}"
-                    });
-                }
-                catch (ExistingUserException ex)
-                {
-                    return BadRequest(new ErrorResponse
-                    {
-                        ErrorType = "UserUpdateError",
-                        Message = $"Erro ao atualizar o usuário: {ex.Message}"
-                    });
-                }
-
-                if (updatedUser == null)
-                    return NotFound(new ErrorResponse
-                    {
-                        ErrorType = "UserNotFound",
-                        Message = "O usuário com o nome fornecido não foi encontrado."
-                    });
-
-                return NoContent();
+                    ErrorType = "ValidationError",
+                    Message = $"Erro de validação: {ex.Message}"
+                });
             }
+            catch (ExistingUserException ex)
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    ErrorType = "UserUpdateError",
+                    Message = $"Erro ao atualizar o usuário: {ex.Message}"
+                });
+            }
+
+            if (updatedUser == null)
+                return NotFound(new ErrorResponse
+                {
+                    ErrorType = "UserNotFound",
+                    Message = "O usuário com o nome fornecido não foi encontrado."
+                });
+
+            return NoContent();
         }
     }
+}
