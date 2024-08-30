36f6d57504d8018c0d22a478ce341afa84dd9401
Sami Daniel <samidaniel125@gmail.com>
2024-08-29 21:54:37 -0300

    Remove eager loading of Expenses and Incomes in GetByIDAsync
    The line `_entitySet.Include("Expenses,Incomes");` was removed from
    the `GetByIDAsync` method in `Repository.cs` within the
    `SmartCapital.WebAPI.Infrastructure.Repository.Core.Implementations`
    namespace. This change stops the eager loading of related entities
    "Expenses" and "Incomes" when retrieving an entity by its ID.

--- SmartCapital.WebAPI.Infrastructure/Repository/Core/Implementations/Repository.cs
+++ SmartCapital.WebAPI.Infrastructure/Repository/Core/Implementations/Repository.cs
@@ -46,7 +46,6 @@ namespace SmartCapital.WebAPI.Infrastructure.Repository.Core.Implementations
 
         public virtual async Task<TEntity?> GetByIDAsync(object ID)
         {
-            _entitySet.Include("Expenses,Incomes");
             return await _entitySet.FindAsync(ID);
         }
 
