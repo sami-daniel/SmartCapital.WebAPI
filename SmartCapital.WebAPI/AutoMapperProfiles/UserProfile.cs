b9417a2466d041f6e9c9d4f3ba1ffa4bf9362c20
Sami Daniel <samidaniel125@gmail.com>
2024-09-08 20:26:22 -0300

    Add UserProfile for mapping UserAddRequest to User
    Introduced a new AutoMapper profile `UserProfile` in the `SmartCapital.WebAPI.AutoMapperProfiles` namespace. This profile maps `UserAddRequest` to `User` while ignoring `UserId`, `UserCreationDate`, and `Profiles` properties. Added necessary using directives for `AutoMapper`, `SmartCapital.WebAPI.Domain.Domain`, and `SmartCapital.WebAPI.DTO.AddRequests`.

---
+++ SmartCapital.WebAPI/AutoMapperProfiles/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SmartCapital.WebAPI.Domain.Domain;
+using SmartCapital.WebAPI.DTO.AddRequests;
+using Profile = AutoMapper.Profile;
+
+namespace SmartCapital.WebAPI.AutoMapperProfiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UserAddRequest, User>()
+               .ForMember(dest => dest.UserId, opt => opt.Ignore())
+               .ForMember(dest => dest.UserCreationDate, opt => opt.Ignore())
+               .ForMember(dest => dest.Profiles, opt => opt.Ignore());
+    }
+}
