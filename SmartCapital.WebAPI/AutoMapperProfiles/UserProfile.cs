de9da8d876c38dc5f79a4da230c16771acc9fc5d
Sami Daniel <samidaniel125@gmail.com>
2024-09-08 20:30:57 -0300

    Add mapping configuration to UserProfile class
    Updated UserProfile class in UserProfile.cs to include a new mapping configuration. Added a new using directive for SmartCapital.WebAPI.DTO.Responses. Implemented a new mapping to map from User to UserResponse, specifying how to map UserName, UserCreationDate, and Profiles properties. Profiles property is mapped by selecting each profile and creating a new UserResponse object with UserName and UserCreationDate mapped from ProfileName and ProfileCreationDate respectively.

--- SmartCapital.WebAPI/AutoMapperProfiles/UserProfile.cs
+++ SmartCapital.WebAPI/AutoMapperProfiles/UserProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using SmartCapital.WebAPI.Domain.Domain;
 using SmartCapital.WebAPI.DTO.AddRequests;
+using SmartCapital.WebAPI.DTO.Responses;
 using Profile = AutoMapper.Profile;
 
 namespace SmartCapital.WebAPI.AutoMapperProfiles;
@@ -13,5 +14,14 @@ public class UserProfile : Profile
                .ForMember(dest => dest.UserId, opt => opt.Ignore())
                .ForMember(dest => dest.UserCreationDate, opt => opt.Ignore())
                .ForMember(dest => dest.Profiles, opt => opt.Ignore());
+
+        CreateMap<User, UserResponse>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.UserCreationDate, opt => opt.MapFrom(src => src.UserCreationDate))
+                .ForMember(dest => dest.Profiles, opt => opt.MapFrom(src => src.Profiles.Select(u => new UserResponse
+                {
+                    UserName = u.ProfileName,
+                    UserCreationDate = u.ProfileCreationDate
+                })));
     }
 }
