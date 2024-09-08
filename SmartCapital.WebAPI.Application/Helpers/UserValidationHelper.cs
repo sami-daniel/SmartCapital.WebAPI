b8f2f3f915e70a18a7353e6d6765cf9f68728ba7
Sami Daniel <samidaniel125@gmail.com>
2024-09-07 22:28:02 -0300

    Add UserValidationHelper for user validation
    Added a new helper class `UserValidationHelper` in the `SmartCapital.WebAPI.Application.Helpers` namespace. This class includes a static method `ValidateUser` which validates a `User` object before it is added to the system. The `ValidateUser` method:
    - Throws an `ArgumentNullException` if the `user` object is null.
    - Throws an `ArgumentException` if the `UserName` or `UserPassword` properties are null or whitespace.
    - Ensures the `UserName` does not exceed 255 characters.
    - Ensures the `UserName` contains only letters, numbers, and spaces using a regular expression.
    
    Added necessary `using` directives for `System.Text.RegularExpressions` and `SmartCapital.WebAPI.Domain.Domain`.

---
+++ SmartCapital.WebAPI.Application/Helpers/UserValidationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using SmartCapital.WebAPI.Domain.Domain;
+
+namespace SmartCapital.WebAPI.Application.Helpers;
+internal static class UserValidationHelper
+{
+    /// <summary>
+    /// Valida um usuário antes de ser adicionado ao sistema.
+    /// </summary>
+    /// <param name="user">O usuário a ser validado.</param>
+    /// <exception cref="ArgumentNullException">Lançada quando o usuário a ser adicionado é nulo.</exception>
+    /// <exception cref="ArgumentException">Lançada quando o nome de usuário ou senha é inválido.</exception>
+    internal static void ValidateUser(User user)
+    {
+        ArgumentNullException.ThrowIfNull(user, nameof(user));
+
+        ArgumentException.ThrowIfNullOrWhiteSpace(user.UserName, nameof(user.UserName));
+        ArgumentException.ThrowIfNullOrWhiteSpace(user.UserPassword, nameof(user.UserPassword));
+
+        if (user.UserName.Length > 255)
+            throw new ArgumentException("O tamanho do nome de usuário não pode exceder 255 caracteres.");
+
+        if (!Regex.Match(user.UserName, "^[a-zA-Z0-9 ]*$").Success)
+        {
+            throw new ArgumentException("O nome de usuário pode conter apenas letras, números e espaços.");
+        }
+
+    }
+}
